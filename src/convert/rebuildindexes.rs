f1bcc24e11f1921010a9bc6484ae4bc3ad1bcddd
James Pharaoh <james@pharaoh.uk>
2017-01-24 12:44:33 +0100

    parallelise rebuild-indexes

--- src/convert/rebuildindexes.rs
+++ src/convert/rebuildindexes.rs
@@ -1,6 +1,14 @@
+use std::collections::LinkedList;
 use std::path::PathBuf;
 
 use clap;
+
+use futures::BoxFuture;
+use futures::Future;
+
+use futures_cpupool::CpuPool;
+
+use num_cpus;
 
 use output::Output;
 
@@ -12,144 +20,225 @@ use ::read::*;
 use ::zbackup::data::*;
 use ::zbackup::proto;
 
-pub fn rebuild_indexes (
-	output: & Output,
-	arguments: & RebuildIndexesArguments,
-) -> Result <bool, String> {
+struct IndexRebuilder <'a> {
+	arguments: & 'a RebuildIndexesArguments,
+	repository: Repository,
+	num_threads: usize,
+	cpu_pool: CpuPool,
+}
 
-	// open repository
+impl <'a> IndexRebuilder <'a> {
 
-	let repository =
-		string_result_with_prefix (
-			|| format! (
-				"Error opening repository {}: ",
-				arguments.repository_path.to_string_lossy ()),
-			Repository::open (
-				& output,
-				Repository::default_config (),
-				& arguments.repository_path,
-				arguments.password_file_path.clone ()),
-		) ?;
+	fn new (
+		output: & Output,
+		arguments: & 'a RebuildIndexesArguments,
+	) -> Result <IndexRebuilder <'a>, String> {
 
-	// begin transaction
+		// open repository
 
-	let mut temp_files =
-		TempFileManager::new (
-			output,
-			& arguments.repository_path,
-			None,
-		) ?;
-
-	// get list of bundle files
-
-	let bundle_ids =
-		scan_bundle_files (
-			output,
-			& arguments.repository_path,
-		) ?;
-
-	output.message_format (
-		format_args! (
-			"Found {} bundle files",
-			bundle_ids.len ()));
-
-	// rebuild indexes
-
-	let mut entries_buffer: Vec <IndexEntry> =
-		Vec::new ();
-
-	let mut bundle_count: u64 = 0;
-
-	output.status (
-		"Rebuilding indexes");
-
-	for & bundle_id in bundle_ids.iter () {
-
-		output.status_progress (
-			bundle_count,
-			bundle_ids.len () as u64);
-
-		let bundle_path =
-			repository.bundle_path (
-				bundle_id);
-
-		let bundle_info =
-			read_bundle_info (
-				bundle_path,
-				repository.encryption_key (),
+		let repository =
+			string_result_with_prefix (
+				|| format! (
+					"Error opening repository {}: ",
+					arguments.repository_path.to_string_lossy ()),
+				Repository::open (
+					& output,
+					Repository::default_config (),
+					& arguments.repository_path,
+					arguments.password_file_path.clone ()),
 			) ?;
 
-		let mut index_bundle_header =
-			proto::IndexBundleHeader::new ();
+		// create thread pool
 
-		index_bundle_header.set_id (
-			bundle_id.to_vec ());
+		let num_threads =
+			num_cpus::get () * 2;
 
-		entries_buffer.push (
-			(
-				index_bundle_header,
-				bundle_info,
-			)
-		);
+		let cpu_pool =
+			CpuPool::new (
+				num_threads);
 
-		// write out a new
+		// return
 
-		if entries_buffer.len () as u64 == arguments.bundles_per_index {
+		Ok (IndexRebuilder {
+			arguments: arguments,
+			repository: repository,
+			num_threads: num_threads,
+			cpu_pool: cpu_pool,
+		})
+
+	}
+
+	fn rebuild_indexes (
+		& mut self,
+		output: & Output,
+	) -> Result <bool, String> {
+
+		// begin transaction
+
+		let mut temp_files =
+			TempFileManager::new (
+				output,
+				& self.arguments.repository_path,
+				None,
+			) ?;
+
+		// get list of bundle files
+
+		let bundle_ids =
+			scan_bundle_files (
+				output,
+				& self.arguments.repository_path,
+			) ?;
+
+		output.message_format (
+			format_args! (
+				"Found {} bundle files",
+				bundle_ids.len ()));
+
+		// rebuild indexes
+
+		let mut entries_buffer: Vec <IndexEntry> =
+			Vec::new ();
+
+		let mut bundle_count: u64 = 0;
+		let bundle_total = bundle_ids.len () as u64;
+
+		output.status (
+			"Rebuilding indexes");
+
+		type BundleFuture =
+			BoxFuture <(BundleId, proto::BundleInfo), String>;
+
+		let mut bundle_futures: LinkedList <BundleFuture> =
+			LinkedList::new ();
+
+		let mut bundle_ids_iter =
+			bundle_ids.into_iter ();
+
+		loop {
+
+			// start bundle load tasks
+
+			while bundle_futures.len () < self.num_threads {
+
+				if let Some (bundle_id) =
+					bundle_ids_iter.next () {
+
+					let repository =
+						self.repository.clone ();
+
+					bundle_futures.push_back (
+						self.cpu_pool.spawn_fn (move ||
+							read_bundle_info (
+								repository.bundle_path (
+									bundle_id),
+								repository.encryption_key (),
+							).map (|bundle_info|
+								(bundle_id, bundle_info)
+							)
+						).boxed ()
+					);
+
+				} else {
+					break;
+				}
+
+			}
+
+			// handle next bundle load
+
+			// TODO use select for better parallelism
+
+			if let Some (bundle_future) =
+				bundle_futures.pop_front () {
+
+				output.status_progress (
+					bundle_count,
+					bundle_total);
+
+				let (bundle_id, bundle_info) =
+					bundle_future.wait () ?;
+
+				let mut index_bundle_header =
+					proto::IndexBundleHeader::new ();
+
+				index_bundle_header.set_id (
+					bundle_id.to_vec ());
+
+				entries_buffer.push (
+					(
+						index_bundle_header,
+						bundle_info,
+					)
+				);
+
+				// write out a new index
+
+				if entries_buffer.len () as u64
+					== self.arguments.bundles_per_index {
+
+					flush_index_entries (
+						& self.repository,
+						& mut temp_files,
+						& mut entries_buffer,
+					) ?;
+
+				}
+
+				bundle_count += 1;
+
+			} else {
+				break;
+			}
+
+		}
+
+		// write out final index
+
+		if ! entries_buffer.is_empty () {
 
 			flush_index_entries (
-				& repository,
+				& self.repository,
 				& mut temp_files,
 				& mut entries_buffer,
 			) ?;
 
 		}
 
-		bundle_count += 1;
+		output.status_done ();
+
+		// remove old indexes
+
+		let old_index_ids =
+			scan_index_files (
+				& self.arguments.repository_path,
+			) ?;
+
+		output.message_format (
+			format_args! (
+				"Removing {} old index files",
+				old_index_ids.len ()));
+
+		for old_index_id in old_index_ids {
+
+			temp_files.delete (
+				self.repository.index_path (
+					old_index_id));
+
+		}
+
+		// commit changes and return
+
+		output.status (
+			"Committing changes ...");
+
+		temp_files.commit () ?;
+
+		output.status_done ();
+
+		Ok (true)
 
 	}
-
-	if ! entries_buffer.is_empty () {
-
-		flush_index_entries (
-			& repository,
-			& mut temp_files,
-			& mut entries_buffer,
-		) ?;
-
-	}
-
-	output.status_done ();
-
-	// remove old indexes
-
-	let old_index_ids =
-		scan_index_files (
-			& arguments.repository_path,
-		) ?;
-
-	output.message_format (
-		format_args! (
-			"Removing {} old index files",
-			old_index_ids.len ()));
-
-	for old_index_id in old_index_ids {
-
-		temp_files.delete (
-			repository.index_path (
-				old_index_id));
-
-	}
-
-	// commit changes and return
-
-	output.status (
-		"Committing changes ...");
-
-	temp_files.commit () ?;
-
-	output.status_done ();
-
-	Ok (true)
 
 }
 
@@ -225,7 +314,12 @@ command! (
 	},
 
 	action = |output, arguments| {
-		rebuild_indexes (output, arguments)
+		IndexRebuilder::new (
+			output,
+			arguments,
+		) ?.rebuild_indexes (
+			output,
+		)
 	},
 
 );
