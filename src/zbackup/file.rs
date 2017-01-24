9fe6db3541089128ca661a7cf3d938b26699b5ba
James Pharaoh <james@pharaoh.uk>
2017-01-24 10:28:35 +0100

    fix temp file renaming using join instead of with_extension, refactor

--- src/zbackup/file.rs
+++ src/zbackup/file.rs
@@ -371,7 +371,7 @@ impl TempFileManager {
 					parent_dir),
 			) ?;
 
-			io_result_with_prefix (
+			string_result_with_prefix (
 				|| format! (
 					"Error renaming temp file {} to {}: ",
 					temp_file_name,
@@ -462,10 +462,19 @@ fn rename_or_copy_and_delete <
 > (
 	source_path: SourcePath,
 	target_path: TargetPath,
-) -> Result <(), io::Error> {
+) -> Result <(), String> {
 
-	let source_path = source_path.as_ref ();
-	let target_path = target_path.as_ref ();
+	rename_or_copy_and_delete_impl (
+		source_path.as_ref (),
+		target_path.as_ref (),
+	)
+
+}
+
+fn rename_or_copy_and_delete_impl (
+	source_path: & Path,
+	target_path: & Path,
+) -> Result <(), String> {
 
 	// try a simple rename
 
@@ -484,26 +493,52 @@ fn rename_or_copy_and_delete <
 		let mut source =
 			File::open (
 				source_path,
+			).map_err (|error|
+				format! (
+					"Error opening source file: {}",
+					error)
 			) ?;
 
 		let target_temp_path =
-			target_path.join (".temp");
+			target_path.with_extension (
+				"temp");
 
 		let mut target_temp =
 			File::create (
 				& target_temp_path,
+			).map_err (|error|
+				format! (
+					"Error opening target temp file {}: {}",
+					target_temp_path.to_string_lossy (),
+					error)
 			) ?;
 
 		io::copy (
 			& mut source,
 			& mut target_temp,
+		).map_err (|error|
+			format! (
+				"Error copying contents to target temp file {}: {}",
+				target_temp_path.to_string_lossy (),
+				error)
 		) ?;
 
-		target_temp.sync_all () ?;
+		target_temp.sync_all (
+		).map_err (|error|
+			format! (
+				"Error syncing target temp file {}: {}",
+				target_temp_path.to_string_lossy (),
+				error)
+		) ?;
 
 		fs::rename (
-			target_temp_path,
+			& target_temp_path,
 			target_path,
+		).map_err (|error|
+			format! (
+				"Error renaming temp file {}: {}",
+				target_temp_path.to_string_lossy (),
+				error)
 		) ?;
 
 	}
@@ -512,6 +547,10 @@ fn rename_or_copy_and_delete <
 
 	fs::remove_file (
 		source_path,
+	).map_err (|error|
+		format! (
+			"Error removing source file: {}",
+			error)
 	) ?;
 
 	Ok (())
